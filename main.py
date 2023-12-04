da488e4aa577b6fc01bd819f03483e04cc932288
Leandro Spagnolo <leandrospagnolo5@gmail.com>
2023-12-04 17:51:13 -0300

    Cambiado nombre variables python

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
 import sys
 import random
 
-def procesarPalabrasEntrada(archivoEntradas):
+def procesarPalabrasEntradas(archivoEntradas):
     
-    diccionario = {}
+    diccionarioPalabras = {}
 
     for linea in archivoEntradas.readlines():
+        
         listaPalabras = linea.split()
-        largo = len(listaPalabras)
-        for posicion in range(largo):
+        largoOracion = len(listaPalabras)
+
+        for posicion in range(largoOracion):
+
             palabraActual = listaPalabras[posicion]
             
-            if palabraActual not in diccionario:
-                diccionario[palabraActual] = {"siguiente": {}, "anterior": {}}
+            if palabraActual not in diccionarioPalabras:
 
-            if posicion + 1 != largo:
+                diccionarioPalabras[palabraActual] = {"siguiente": {}, "anterior": {}}
+
+            if posicion + 1 != largoOracion:
+
                 palabraSiguiente = listaPalabras[posicion + 1]
-                if palabraSiguiente not in diccionario[palabraActual]["siguiente"]:
-                    diccionario[palabraActual]["siguiente"][palabraSiguiente] = 0
-                diccionario[palabraActual]["siguiente"][palabraSiguiente] += 1
+
+                if palabraSiguiente not in diccionarioPalabras[palabraActual]["siguiente"]:
+                    diccionarioPalabras[palabraActual]["siguiente"][palabraSiguiente] = 0
+
+                diccionarioPalabras[palabraActual]["siguiente"][palabraSiguiente] += 1
 
             if posicion > 0:
+
                 palabraAnterior = listaPalabras[posicion - 1]
-                if palabraAnterior not in diccionario[palabraActual]["anterior"]:
-                    diccionario[palabraActual]["anterior"][palabraAnterior] = 0
-                diccionario[palabraActual]["anterior"][palabraAnterior] += 1
 
-    return diccionario    
+                if palabraAnterior not in diccionarioPalabras[palabraActual]["anterior"]:
+                    diccionarioPalabras[palabraActual]["anterior"][palabraAnterior] = 0
+
+                diccionarioPalabras[palabraActual]["anterior"][palabraAnterior] += 1
+
+    return diccionarioPalabras   
 
 
-def adivinarPalabra(diccionario):
-    listaLlaves = list(diccionario.keys())
+def adivinarPalabra(diccionarioPalabra):
+
+    listaLlaves = list(diccionarioPalabra.keys())
     palabraFinal = listaLlaves[0]
 
-    for palabra in diccionario:
-        if diccionario[palabra] > diccionario[palabraFinal]:
+    for palabra in diccionarioPalabra:
+        if diccionarioPalabra[palabra] > diccionarioPalabra[palabraFinal]:
             palabraFinal = palabra
 
     return palabraFinal
@@ -42,19 +53,28 @@ def adivinarPalabra(diccionario):
 
 
 def completarArchivoSalida(diccionario, archivoFrases, archivoSalida):
+
     for linea in archivoFrases.readlines():
+
         listaPalabras = linea.split()
         largo = len(listaPalabras)
 
         for posicion in range(largo):
+
             palabraActual = listaPalabras[posicion]
+
             if '_' in palabraActual:
+
                 if posicion > 0 and listaPalabras[posicion - 1] in diccionario:
+
                     palabraAnterior = listaPalabras[posicion - 1]
                     listaPalabras[posicion] = adivinarPalabra(diccionario[palabraAnterior]["siguiente"])
+
                 elif posicion + 1 < largo and listaPalabras[posicion + 1] in diccionario:
+
                     palabraSiguiente = listaPalabras[posicion   + 1]
                     listaPalabras[posicion] = adivinarPalabra(diccionario[palabraSiguiente]["anterior"])
+                    
                 else:
                     palabraAleatoria = random.choice(list(diccionario.keys()))
                     listaPalabras[posicion] = palabraAleatoria
@@ -77,7 +97,7 @@ def main():
 
     archivoSalidas = open("./Salidas/" + nombreArtista + ".txt", "w") 
     
-    diccionarioPalabras = procesarPalabrasEntrada(archivoEntradas)
+    diccionarioPalabras = procesarPalabrasEntradas(archivoEntradas)
     
     completarArchivoSalida(diccionarioPalabras,archivoFrases,archivoSalidas)
 
