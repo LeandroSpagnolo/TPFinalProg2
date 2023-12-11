472a8025a7cc3f8031895f0e0d4640e5fda83044
Leandro Spagnolo <leandrospagnolo5@gmail.com>
2023-12-11 16:15:11 -0300

    Arreglado palabras juntas

--- main.c
+++ main.c
@@ -56,6 +56,10 @@ void procesarTextosDelArtista(char *nombreTexto,char *nombreArtista, FILE *archi
             }
         }
         
+        if (caracter == '\n' && caracterPrevio != '.') {
+            fputc(' ', archivosalida);
+        }
+        
         if (caracter == '.') {
             fputc('\n', archivosalida);
         }
